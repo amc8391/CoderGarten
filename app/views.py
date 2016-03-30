38037f09924eb609eb883f398b4f938e20290ee5
Derek Leung <dxl3349@rit.edu>
2016-03-30 17:35:03 -0400

    fixed size of content div

--- app/views.py
+++ app/views.py
@@ -7,4 +7,8 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+@app.route('/student-homepage')
+def student_homepage():
+	return render_template('student-homepage.html')
+
 
