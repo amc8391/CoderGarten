c4eb6167b635844ea8c628814d17253c66850892
Derek Leung <dxl3349@rit.edu>
2016-04-04 12:54:22 -0400

    -implemented functionality for logging in with student, teacher, and parent on login.html -fixed margin style

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -6,11 +6,39 @@ $(document).ready( function () {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
+
 	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
 			$("#login-form").fadeOut(100);
 		$('#login-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
+	});
+
+	$('#login-submit').click(function(e){
+		var username = $('#username').val();
+		var password = $('#password').val();
+		var valid = false;
+		var url = "http://localhost:5000"
+		var pString = "?username=" + username + "&password=" + password;
+
+		if(username == 'saitama' && password == 'onepunch'){
+			url += "/student-homepage";
+			pString += "&type=student";
+			valid = true;
+		} else if (username == 'one' && password == 'mobpsycho'){
+			url += "/parent-homepage";
+			pString += "&type=parent";
+			valid = true;
+		}
+		else if (username == 'murata' && password == 'redraw'){
+			url += "/teacher-homepage";
+			pString += "&type=teacher";
+			valid = true;
+		}
+		if(valid == true){
+			window.location.href = url + pString;
+		}
+		
 	});
 });
