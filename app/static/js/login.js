9e7be7f72d71609a9082d5b802b2795654a155f6
Derek Leung <dxl3349@rit.edu>
2016-04-04 17:19:06 -0400

    implemented login submit when 'enter' key is pressed on login.html

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,3 +1,30 @@
+function loginSubmit(){
+	var username = $('#username').val();
+	var password = $('#password').val();
+	var valid = false;
+	var url = "http://localhost:5000"
+
+	if(username == 'saitama' && password == 'onepunch'){
+		sessionStorage.setItem("uType","student");
+		url += "/student-homepage";
+		valid = true;
+	} else if (username == 'one' && password == 'mobpsycho'){
+		sessionStorage.setItem("uType","parent");
+		url += "/parent-homepage";
+		valid = true;
+	}
+	else if (username == 'murata' && password == 'redraw'){
+		sessionStorage.setItem("uType","teacher");
+		url += "/teacher-homepage";
+		valid = true;
+	} else {
+		$('#incorrect-creds').removeClass('login-error');
+	}
+	if(valid == true){
+		window.location.href = url;
+	}
+}
+
 $(document).ready( function () {
 	/* show hide elements*/
 
@@ -19,30 +46,15 @@ $(document).ready( function () {
 	});
 
 	$('#login-submit').click(function(e){
-		var username = $('#username').val();
-		var password = $('#password').val();
-		var valid = false;
-		var url = "http://localhost:5000"
+		loginSubmit();
+	});
 
-		if(username == 'saitama' && password == 'onepunch'){
-			sessionStorage.setItem("uType","student");
-			url += "/student-homepage";
-			valid = true;
-		} else if (username == 'one' && password == 'mobpsycho'){
-			sessionStorage.setItem("uType","parent");
-			url += "/parent-homepage";
-			valid = true;
-		}
-		else if (username == 'murata' && password == 'redraw'){
-			sessionStorage.setItem("uType","teacher");
-			url += "/teacher-homepage";
-			valid = true;
-		} else {
-			$('#incorrect-creds').removeClass('login-error');
-		}
-		if(valid == true){
-			window.location.href = url;
-		}
-		
+	$(document).keypress(function(e){
+		var key = (event.keyCode ? event.keyCode : event.which);
+		 if(key == 13)  // the enter key code
+		  {
+		    $('input[id = login-submit]').click();
+		    return false;  
+		  }
 	});
 });
