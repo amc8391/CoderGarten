7b44a5d042d433c45ec052d864f8f59d8c2778fa
Derek Leung <dxl3349@rit.edu>
2016-04-10 00:15:41 -0400

    added files and made changes to run.py for heroku deploy

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 __author__ = 'Team OnePunch'
 
 #!flask/bin/python
-from app import app
+import os
+from flask import Flask
+
+app = Flask(__name__)
 #app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
