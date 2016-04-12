0c8eeb791d5d81e856d64f018e45d55f0c4effba
Alex Christodoulou <amc8391@rit.edu>
2016-04-12 12:05:24 -0400

    Added student editor
    Parent and teacher need to be added

--- app/static/js/editor.js
+++ app/static/js/editor.js
@@ -1,3 +1,7 @@
+var CURRENT_EDITOR;
+
+
+
 //http://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-url-parameter
 function getUrlVars() {
     var vars = {};
@@ -15,9 +19,33 @@ function builtinRead(x) {
 
 
 function outf(text) { 
-    var mypre = document.getElementById("editor_output"); 
+    var mypre = document.getElementById("output"); 
     mypre.innerHTML = mypre.innerHTML + text; 
 } 
+
+function displayAttack(){
+  //   <img src="http://vignette2.wikia.nocookie.net/onepunchman/images/6/64/Vaccine_Man.png/revision/latest?cb=20121129002142" height="100px" width="150px">
+  // <img src="https://media.giphy.com/media/3o85xwgtcuADKQ8QxO/giphy.gif" height="100px" width="150px">
+  // <img src="https://i.kinja-img.com/gawker-media/image/upload/jcc53xkcdkbezhceppdr.gif" height="100px" width="150px">
+  // <img src="https://media.giphy.com/media/arbHBoiUWUgmc/giphy.gif" height="100px" width="150px">
+
+  var attack_imgs = [
+    "https://media.giphy.com/media/3o85xwgtcuADKQ8QxO/giphy.gif",
+    "https://i.kinja-img.com/gawker-media/image/upload/jcc53xkcdkbezhceppdr.gif",
+    "https://media.giphy.com/media/arbHBoiUWUgmc/giphy.gif"
+  ]
+  var boss_img = document.getElementById("bossimg");
+  boss_img.src = attack_imgs[getRandomIntInclusive(0, attack_imgs.length-1)];
+}
+
+function displayBoss(){
+  var boss_img = document.getElementById("bossimg");
+  boss_img.src = "http://vignette2.wikia.nocookie.net/onepunchman/images/6/64/Vaccine_Man.png/revision/latest?cb=20121129002142";
+}
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Here's everything you need to run a python program in skulpt
 // grab the code from your textarea
@@ -25,8 +53,10 @@ function outf(text) {
 // configure the output function
 // call Sk.importMainWithBody()
 function runit() { 
-   var prog = document.getElementById("editor_input").value; 
-   var mypre = document.getElementById("editor_output"); 
+   displayAttack();
+
+   var prog = CURRENT_EDITOR.getValue(); 
+   var mypre = document.getElementById("output"); 
    mypre.innerHTML = ''; 
    Sk.pre = "output";
    Sk.configure({output:outf, read:builtinRead}); 
@@ -39,15 +69,20 @@ function runit() {
    },
        function(err) {
        console.log(err.toString());
+       mypre.innerHTML = err.toString();
    });
+
+    displayBoss();
 } 
 
 function startEditor() {
-    myTextarea = document.getElementById("editor_input");
-    var editor = CodeMirror.fromTextArea(myTextarea, {
+    myTextarea = document.getElementById("yourcode");
+    CURRENT_EDITOR = CodeMirror.fromTextArea(myTextarea, {
         lineNumbers: true,
         mode: 'python'
     });
+
+    displayBoss();
 }
 
 $(document).ready(function() {
@@ -56,6 +91,23 @@ $(document).ready(function() {
     console.log("loaded it!");
     startEditor();
 
+    //sessionStorage.getItems('utype')
+    var uType = sessionStorage.getItem("uType");
+    switch(uType){
+      case "student":
+        $('#student-nav').removeClass('student-nav');
+        break;
+
+      case "teacher":
+        $('#teacher-nav').removeClass('teacher-nav');
+        break;
+
+      case "parent":
+        $('#parent-nav').removeClass('parent-nav');
+        break;
+      case "default":
+        break;
+    }
     // var userClass = {'.student', '.teacher', '.parent'};
     // if (userType === "student") {
     // } else if (userType === "teacher") {
