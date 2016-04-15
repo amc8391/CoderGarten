6b858aa5aded2a64db0b3472c1e4b056e4da2e25
Alex Christodoulou <amc8391@rit.edu>
2016-04-15 06:25:38 -0400

    3 layouts are included
    Teacher, parent, and student layouts are there but not differentiated yet
    They don't show up at the same time though, so that's a plus!

--- app/static/js/editor.js
+++ app/static/js/editor.js
@@ -1,16 +1,5 @@
 var CURRENT_EDITOR;
 
-
-
-//http://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-url-parameter
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
-        function(m, key, value) {
-            vars[key] = value;
-        });
-    return vars;
-}
 function builtinRead(x) {
     if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
             throw "File not found: '" + x + "'";
@@ -19,28 +8,32 @@ function builtinRead(x) {
 
 
 function outf(text) { 
-    var mypre = document.getElementById("output"); 
-    mypre.innerHTML = mypre.innerHTML + text; 
+
+    $( ".output" ).each(function(){this.innerHTML = this.innerHTML + text});
+
+    // var mypre = document.getElementById("output"); 
+    // mypre.innerHTML = mypre.innerHTML + text; 
 } 
 
 function displayAttack(){
-  //   <img src="http://vignette2.wikia.nocookie.net/onepunchman/images/6/64/Vaccine_Man.png/revision/latest?cb=20121129002142" height="100px" width="150px">
-  // <img src="https://media.giphy.com/media/3o85xwgtcuADKQ8QxO/giphy.gif" height="100px" width="150px">
-  // <img src="https://i.kinja-img.com/gawker-media/image/upload/jcc53xkcdkbezhceppdr.gif" height="100px" width="150px">
-  // <img src="https://media.giphy.com/media/arbHBoiUWUgmc/giphy.gif" height="100px" width="150px">
-
   var attack_imgs = [
     "https://media.giphy.com/media/3o85xwgtcuADKQ8QxO/giphy.gif",
     "https://i.kinja-img.com/gawker-media/image/upload/jcc53xkcdkbezhceppdr.gif",
     "https://media.giphy.com/media/arbHBoiUWUgmc/giphy.gif"
   ]
-  var boss_img = document.getElementById("bossimg");
-  boss_img.src = attack_imgs[getRandomIntInclusive(0, attack_imgs.length-1)];
+  $( ".bossimg" ).each(function(){
+    this.src = attack_imgs[getRandomIntInclusive(0, attack_imgs.length-1)];
+  });
+  $( ".progress-bar" ).each(function(){
+    $(this).css('width', '33%');
+    this.innerText = '33%';
+  });
 }
 
 function displayBoss(){
-  var boss_img = document.getElementById("bossimg");
-  boss_img.src = "http://vignette2.wikia.nocookie.net/onepunchman/images/6/64/Vaccine_Man.png/revision/latest?cb=20121129002142";
+  $( ".bossimg" ).each(function(){
+     this.src = "http://vignette2.wikia.nocookie.net/onepunchman/images/6/64/Vaccine_Man.png/revision/latest?cb=20121129002142";
+  });
 }
 
 function getRandomIntInclusive(min, max) {
@@ -55,9 +48,10 @@ function getRandomIntInclusive(min, max) {
 function runit() { 
    displayAttack();
 
-   var prog = CURRENT_EDITOR.getValue(); 
-   var mypre = document.getElementById("output"); 
-   mypre.innerHTML = ''; 
+   var prog = CURRENT_EDITOR.getValue();
+   //var mypre = document.getElementById("output");
+   $( ".output" ).each(function(){this.innerHTML = ""});
+   //mypre.innerHTML = ''; 
    Sk.pre = "output";
    Sk.configure({output:outf, read:builtinRead}); 
    (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
@@ -69,51 +63,58 @@ function runit() {
    },
        function(err) {
        console.log(err.toString());
-       mypre.innerHTML = err.toString();
+       $( ".output" ).each(function(){this.innerHTML = err.toString()});
+       //mypre.innerHTML = err.toString();
    });
 
-    displayBoss();
+    //displayBoss().slideUp(300).delay(300);
 } 
 
 function startEditor() {
-    myTextarea = document.getElementById("yourcode");
-    CURRENT_EDITOR = CodeMirror.fromTextArea(myTextarea, {
-        lineNumbers: true,
-        mode: 'python'
+    $( ".yourcode" ).each(function(){
+      if($(this).is(":visible")){
+        CURRENT_EDITOR = CodeMirror.fromTextArea(this), {
+          lineNumbers: true,
+          mode: 'python'
+        }
+      }
     });
+    // myTextarea = document.getElementById("yourcode");
+    // CURRENT_EDITOR = CodeMirror.fromTextArea(myTextarea, {
+    //     lineNumbers: true,
+    //     mode: 'python'
+    // });
 
-    displayBoss();
 }
 
 $(document).ready(function() {
-    var userType = getUrlVars()["type"];
-    console.log(userType);
-    console.log("loaded it!");
-    startEditor();
-
-    //sessionStorage.getItems('utype')
+    /* decide which editor to show */
     var uType = sessionStorage.getItem("uType");
     switch(uType){
       case "student":
-        $('#student-nav').removeClass('student-nav');
+        $('#student-editor').removeClass('student-editor');
+        $('#teacher-editor').remove();
+        $('#parent-editor').remove();
         break;
 
       case "teacher":
-        $('#teacher-nav').removeClass('teacher-nav');
+        $('#teacher-editor').removeClass('teacher-editor');
+        $('#student-editor').remove();
+        $('#parent-editor').remove();
         break;
 
       case "parent":
-        $('#parent-nav').removeClass('parent-nav');
+        $('#parent-editor').removeClass('parent-editor');
+        $('#teacher-editor').remove();
+        $('#student-editor').remove();
         break;
       case "default":
         break;
     }
-    // var userClass = {'.student', '.teacher', '.parent'};
-    // if (userType === "student") {
-    // } else if (userType === "teacher") {
 
-    // } else if (userType === "parent") {
-
-    // }
+    //console.log(userType);
+    console.log("loaded it!");
+    startEditor();
+    displayBoss();
 });
 // if userType ==
