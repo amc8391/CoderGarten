6e938dd77a84f38430a86fd3c529eed232a16ea8
Zachary Yaro <zmyaro@zmyaro.com>
2016-05-05 13:38:37 -0400

    Highlight nav bar button for current page

--- app/views.py
+++ app/views.py
@@ -16,32 +16,32 @@ def login():
 
 @app.route('/student-homepage')
 def student_homepage():
-    return render_template('student-homepage.html')
+    return render_template('student-homepage.html', active_page='home')
 
 @app.route('/student-assignments')
 def student_assignments():
-    return render_template('student-assignments.html')
+    return render_template('student-assignments.html', active_page='assignments')
 
 # TODO: Replace later.
 @app.route('/parent-homepage')
 def parent_homepage():
-    return render_template('parent-homepage.html')
+    return render_template('parent-homepage.html', active_page='home')
 
 @app.route('/leaderboard')
 def leaderboard():
-    return render_template('leaderboard.html')
+    return render_template('leaderboard.html', active_page='leaderboards')
 
 @app.route('/messages')
 def messages():
-    return render_template('messages.html')
+    return render_template('messages.html', active_page='messages')
 
 @app.route('/message/new')
 def new_message():
-    return render_template('new-message.html')
+    return render_template('new-message.html', active_page='messages')
 
 @app.route('/message/view')
 def view_message():
-    return render_template('view-message.html')
+    return render_template('view-message.html', active_page='messages')
 
 
 @app.route('/teacher-assignments', methods=['GET', 'POST'])
@@ -49,7 +49,7 @@ def teacher_assignments():
     if request.method == "POST":
         session['modal'].append(request.form['modal'])
 
-    return render_template('teacher-assignments.html')
+    return render_template('teacher-assignments.html', active_page='assignments')
 
 
 @app.route('/editor')
@@ -59,11 +59,11 @@ def editor():
 
 @app.route('/teacher-homepage')
 def teacher_homepage():
-    return render_template('teacher-homepage.html')
+    return render_template('teacher-homepage.html', active_page='home')
 
 @app.route('/settings')
 def settings():
-    return render_template('settings.html')
+    return render_template('settings.html', active_page='settings')
 
 @app.route('/class-section')
 def class_section():
